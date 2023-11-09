25604ea2d2e5ce2a88f06c0d2fce4161b6a26ccd
Nathan Asowata <nathanasowata@gmail.com>
2023-11-09 10:22:14 +0000

    sttyle: reduced the font size and the width of the form

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,14 @@
+import SplashScreen from "@/components/SplashScreen"
+
+export default function RegisterLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return(
+        <main className="grid grid-cols-5 min-h-screen">
+            <SplashScreen />
+            {children}
+        </main>
+    )
+  }
