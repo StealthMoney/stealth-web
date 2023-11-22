fb60bbf90b1f5475bb9e26e2a8410bd5934b3b17
Nathan Asowata <nathanasowata@gmail.com>
2023-11-22 09:25:02 +0000

    refactor: layout file

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -12,17 +12,3 @@ export default function RegisterLayout({
         </main>
     )
 }
-import SplashScreen from "@/components/SplashScreen"
-
-export default function RegisterLayout({
-    children
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <main className="grid grid-cols-5 min-h-screen">
-            <SplashScreen />
-            {children}
-        </main>
-    )
-}
