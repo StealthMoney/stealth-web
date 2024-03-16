c3f8ef2b4491e151afd0c8497b6f9aa5ff2bd986
Oladunni Faith <dunni82@yahoo.com>
2024-03-16 14:02:10 -0700

    feat: app responsiveness updates

--- src/app/context/AuthProvider.tsx
+++ src/app/context/AuthProvider.tsx
@@ -1,11 +1,60 @@
 "use client"
 
-import { SessionProvider } from "next-auth/react"
+import jwt_decode from "jwt-decode"
+import { SessionProvider, signOut, useSession } from "next-auth/react"
+import { useEffect } from "react"
+
+import { DecodedJwt } from "@/types/jwt"
+
+export function SessionCheckComponent({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	const { data: session, status } = useSession()
+	const isLoggedOut = status === "unauthenticated"
+
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			if (!session?.accessToken && !isLoggedOut) {
+				signOut({ redirect: false })
+				return
+			}
+
+			if (status === "authenticated") {
+				const decodedToken = jwt_decode(session?.accessToken ?? "") as DecodedJwt
+				if (!decodedToken) {
+					signOut({ redirect: false })
+					return
+				}
+				const expires = new Date((decodedToken.exp ?? 0) * 1000)
+				if (expires < new Date()) {
+					signOut({
+						redirect: false,
+					})
+				}
+			}
+		}, 30000) // check every 30 seconds
+
+		return () => clearInterval(intervalId)
+	}, [session, status, isLoggedOut])
+
+	return <>{children}</>
+}
 
 export default function AuthProvider({
 	children,
 }: {
 	children: React.ReactNode
 }) {
-	return <SessionProvider>{children}</SessionProvider>
+	return (
+		<SessionProvider>
+			<SessionCheckComponent>{children}</SessionCheckComponent>
+		</SessionProvider>
+	)
+	return (
+		<SessionProvider>
+			<SessionCheckComponent>{children}</SessionCheckComponent>
+		</SessionProvider>
+	)
 }
